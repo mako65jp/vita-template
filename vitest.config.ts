6a2bba5f0e8db8a0e2edc5afea18ddc0cfd25e6d
ARAKI Makoto <mako65jp@gmail.com>
2026-08-05 15:06:09 +0900

    カバレッジ　追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -11,7 +11,17 @@ export default defineConfig({
     projects: [
       'packages/*',
       'apps/api',
-      'apps/web/vitest.config.ts',
+      'apps/web',
+      // 'apps/web/vitest.config.ts',
     ],
+
+    // 除外するファイル/フォルダ
+    exclude: ['node_modules', 'dist', '.next', 'coverage'],
+
+    coverage: {
+      provider: 'v8', // or 'istanbul'
+      include: ['**/*.{ts,tsx}']
+    },
+
   },
 });
